f1230ca6dc2f421d5ed41bb0009e0b727e1e7ae8
raylu15742cs <raylucs15742@gmail.com>
2023-08-15 16:22:37 -0700

    update chat

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
