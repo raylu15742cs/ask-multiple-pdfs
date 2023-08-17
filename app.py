9dd736a712fea721e25714115fb81e3c88dada30
raylu15742cs <raylucs15742@gmail.com>
2023-08-17 15:14:34 -0700

    remove comments

--- app.py
+++ app.py
@@ -43,14 +43,12 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = Pinecone.from_documents(text_chunks, embedding=embeddings, index_name=INDEX_NAME)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
