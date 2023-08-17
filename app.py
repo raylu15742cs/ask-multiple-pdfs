e4bffacd316989bf924dd0679357a5dc3d0acd20
raylu15742cs <raylucs15742@gmail.com>
2023-08-17 15:13:20 -0700

    remove unnecessary

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import streamlit as st
 import openai
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
 import pinecone
 
 from langchain.vectorstores import Pinecone
@@ -81,8 +78,6 @@ def handle_userinput(user_question):
 
 
 def main():
-
-    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
